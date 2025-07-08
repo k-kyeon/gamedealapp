2a606f549baa8212a9e76fa5c03a52ba818821d9
k-kyeon <150563468+k-kyeon@users.noreply.github.com>
2025-07-07 22:42:03 -0700

    UI for sign-up/sign-in complete.

--- src/pages/AuthForm.jsx
+++ src/pages/AuthForm.jsx
@@ -19,21 +19,22 @@ import { useState } from "react";
 import { CircleLoader } from "react-spinners";
 import { Link } from "react-router-dom";
 
-const authFormSchema = (formType) => {
-  return z.object({
-    email: z.string().email(),
-    name:
-      formType === "sign-up"
-        ? z.string().min(2).max(50)
-        : z.string().optional(),
-  });
-};
+const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+const signUpSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
 
 const AuthForm = ({ type }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const formSchema = authFormSchema(type);
+  const formSchema = type === "sign-in" ? signInSchema : signUpSchema;
 
   // 1. Define your form.
   const form = useForm({
@@ -57,8 +58,8 @@ const AuthForm = ({ type }) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="flex flex-col w-full min-h-screen bg-blue-100 justify-center items-center border-2 border-red-600"
         >
-          <div className="bg-white p-20 rounded-4xl">
-            <div className="flex flex-col space-y-8">
+          <div className="w-3/4 bg-white p-20 rounded-4xl">
+            <div className="flex flex-col space-y-8 ">
               <h1 className="font-bold">
                 {type === "sign-in" ? "Sign In" : "Sign Up"}
               </h1>
@@ -100,31 +101,30 @@ const AuthForm = ({ type }) => {
               />
             </div>
 
-            <Button
-              type="submit"
-              className="mt-7 w-full self-center rounded-2xl"
-              disabled={isLoading}
-            >
-              {type === "sign-in" ? "Sign In" : "Sign Up"}
+            <div className="flex flex-col items-center">
+              <Button type="submit" className="mt-7 w-3/4" disabled={isLoading}>
+                {type === "sign-in" ? "Sign In" : "Sign Up"}
 
-              {isLoading && <CircleLoader color="#ffffff" size={25} />}
-            </Button>
+                {isLoading && (
+                  <CircleLoader color="#ffffff" size={25} className="ml-2" />
+                )}
+              </Button>
 
-            {errorMessage && <p>{errorMessage}</p>}
+              {errorMessage && <p>{errorMessage}</p>}
 
-            <div className="flex justify-center mt-5">
-              <p className="text-gray-800">
-                {type === "sign-in"
-                  ? "Don't have an account?"
-                  : "Already have an account?"}
-              </p>
-              <Link
-                to={type === "sign-in" ? "/sign-up" : "/sign-in"}
-                className=""
-              >
-                {" "}
-                {type === "sign-in" ? "Sign Up" : "Sign In"}
-              </Link>
+              <div className="flex justify-center mt-5">
+                <p className="text-gray-800">
+                  {type === "sign-in"
+                    ? "Don't have an account?"
+                    : "Already have an account?"}
+                </p>
+                <Link
+                  to={type === "sign-in" ? "/sign-up" : "/sign-in"}
+                  className="ml-1"
+                >
+                  {type === "sign-in" ? "Sign Up" : "Sign In"}
+                </Link>
+              </div>
             </div>
           </div>
         </form>
